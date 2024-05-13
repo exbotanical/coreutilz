56aec458068f203a366771a856658159b89f3b40
Matthew Zito <exbotanical@protonmail.com>
2024-05-13 01:08:52 -0700

    fix: prevent running who test in container

--- t/who_shpec.bash
+++ t/who_shpec.bash
@@ -3,6 +3,11 @@ ROOT_DIR="$(dirname "$(readlink -f $BASH_SOURCE)")"
 # shellcheck source=utils/shpec_utils.bash
 . "$ROOT_DIR/utils/shpec_utils.bash"
 
+# https://stackoverflow.com/questions/60699218/who-and-w-commands-in-centos-8-docker-container
+[[ -z $(cat /proc/self/cgroup | grep -oP '(?<=0::/).*') ]] && {
+  return 0
+}
+
 describe 'who program'
   it 'runs'
     me="$(whoami)"
